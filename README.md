6664b30f134dc7e76e77b440503ae05cf4bc2538
x46807 <x46807@gmail.com>
2019-03-03 10:11:01 -0800

    Added registry and file auditing.  Updated readme.

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
-"# logdkfa" 
+# LOGDKFA
 
-#Powershell script designed to implement enhanced auditing and logging on a system for a specific time period.
+## Purpose
 
+Configuring logging often requires a delicate balance.  Setting the logs to be too verbose can quickly overload system resources.  Insufficient logging can cause important events to be missed.  **LOGDKFA** allows you to throttle up logging and audit settings when needed in a granular fashion.  For example, you could configure more verbose logging based on event triggers such as:
+* File downloads
+* Administrative credentials invoked
+* Alert triggered by SIEM or network sensor
+* New programs installed
+* Program execution (e.g. Powershell, cmd.exe)
 
-#To run:  execute powershell logdkfa.ps1 <time in seconds> from command line
+Note that if you want to set filesystem and/or registry auditing, you need to globally enable object access auditing, a la:
+
+@("Object Access","*","enable","enable")
+
+**LOGDKFA** is designed to enable auditing for a period of time that you specify and then clean up after itself.  Note that the process does not survive being killed or a system reboot, so if it is interrupted before cleanup happens then you will have to restore the original settings manually.  It is recommended that you build configuration files that are tailored to your triggers.  
+
+The default logging, file auditing, and registry auditing settings that are in the configuration files are based on the cheat sheets published by [Malware Archaelology](https://www.malwarearchaeology.com/cheat-sheets) .  Typically these configurations are appropriate for "steady state" operations and are best managed by group policy settings in large organizations.  **LOGDKFA** may be more useful when there is a requirement for very verbose logs (e.g. Windows Filtering Platform) for a short period of time.
+
+## Configuration
+
+Configuration steps are pretty simple:
+* Configure the **audit_settings** array to configure the audit policy settings you want
+* Edit **conf.json** to specify whether you want to enable filesystem auditing, registry auditing and the path for those configuration files.
+* If needed, create your file auditing and registry auditing configurations as per the file_audit.conf and reg_audit.conf templates.   
+* Ensure that the configuration files are placed in the same directory as the script (or the path(s) are passed to the script appropriately)
+
+## Execution
+
+Execute powershell logdkfa.ps1 <time in seconds> as an administrator.
+
+Note that the default time is set to 300 seconds (5 minutes). Also, if you have multiple configuration files (beyond the default **conf.json**) be sure to pass the correct configuration file path as a parameter. 
+
+## Contributions and Future
+
+This script was designed for functionality.  It can be made much prettier with the help of smarter people putting eyes on it.  Contributions are very welcome!
+
+Future plans include:
+* Support for powershell logging and script block auditing
+* Porting the audit_settings array to a conf file to facilitate flexibility
+* Support for enabling special log types (outside of standard Security logs)
+* Better error handling and settings validation
+* A comparable capability for *nix systems 
--- logdkfa.ps1
+++ logdkfa.ps1
@@ -1,21 +1,90 @@
 
 param (  
 [int]$time_period = 300,
-[string]$config_path = ".\default.conf"
+[string]$config_path = ".\conf.json"
 )
+
+
+
+function set_object_audit {
+ param( [string]$object_path, 
+ [string[]]$accounts, 
+ [string[]]$auditrights, 
+ [string[]]$inheritanceflags,
+ [string[]]$propagationflags,
+ [string[]]$auditflags,
+ [string]$type)
+
+<#
+Example:  set_object_audit -object_path $env:userprofile\Desktop -accounts "Everyone" -auditrights "AppendData, ChangePermissions, CreateDirectories, CreateFiles, Delete, DeleteSubdirectoriesAndFiles, TakeOwnership, Write, WriteAttributes, WriteExtendedAttributes" -inheritanceflags "ContainerInherit, ObjectInherit" -propagationflags "NoPropagateInherit" -auditflags "Success" -type "file"
+Example:  set_object_audit -object_path HKCU:\Software\Classes\CLSID -accounts "Everyone" -auditrights "SetValue,CreateSubkey,Delete,ChangePermissions,TakeOwnership" -inheritanceflags "ContainerInherit, ObjectInherit" -propagationflags "NoPropagateInherit" -auditflags "Success" -type "reg"
+FileSystemRights Ref:  https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.filesystemrights?view=netframework-4.7.2
+InheritanceFlags Ref:  https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.inheritanceflags?view=netframework-4.7.2
+PropagationFlags Ref: https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.propagationflags?view=netframework-4.7.2
+AuditFlags Ref:  https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.auditflags?view=netframework-4.7.2
+#>
+
+
+
+if($type -eq "file") {
+$ACL = new-object System.Security.AccessControl.DirectorySecurity
+$AccessRule = new-object System.Security.AccessControl.FileSystemAuditRule($accounts, $auditrights, $inheritanceflags, $propagationflags, $auditflags)
+}
+elseif($type -eq "reg") {
+$ACL = new-object System.Security.AccessControl.RegistrySecurity
+$AccessRule = new-object System.Security.AccessControl.RegistryAuditRule($accounts, $auditrights, $inheritanceflags, $propagationflags, $auditflags)
+}
+$ACL.SetAuditRule($AccessRule)
+$ACL | Set-Acl $object_path -ErrorAction SilentlyContinue
+
+}
+
+function remove_object_audit {
+ param( [string]$object_path, 
+ [string[]]$accounts, 
+ [string[]]$auditrights, 
+ [string[]]$inheritanceflags,
+ [string[]]$propagationflags,
+ [string[]]$auditflags,
+ [string]$type)
+
+<#
+Example:  remove_object_audit -object_path $env:userprofile\Desktop -accounts "Everyone" -auditrights "AppendData, ChangePermissions, CreateDirectories, CreateFiles, Delete, DeleteSubdirectoriesAndFiles, TakeOwnership, Write, WriteAttributes, WriteExtendedAttributes" -inheritanceflags "ContainerInherit, ObjectInherit" -propagationflags "NoPropagateInherit" -auditflags "Success" -type "file"
+FileSystemRights Ref:  https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.filesystemrights?view=netframework-4.7.2
+InheritanceFlags Ref:  https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.inheritanceflags?view=netframework-4.7.2
+PropagationFlags Ref: https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.propagationflags?view=netframework-4.7.2
+AuditFlags Ref:  https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.auditflags?view=netframework-4.7.2
+#>
+
+if($type -eq "file") {
+$ACL = new-object System.Security.AccessControl.DirectorySecurity
+$AccessRule = new-object System.Security.AccessControl.FileSystemAuditRule($accounts, $auditrights, $inheritanceflags, $propagationflags, $auditflags)
+}
+elseif($type -eq "reg") {
+$ACL = new-object System.Security.AccessControl.RegistrySecurity
+$AccessRule = new-object System.Security.AccessControl.RegistryAuditRule($accounts, $auditrights, $inheritanceflags, $propagationflags, $auditflags)
+}
+$ACL.RemoveAuditRule($AccessRule)
+$ACL | Set-Acl $object_path -ErrorAction SilentlyContinue
+
+}
+
+#Read from config file and convert to hash table
+$conf = $(gc $config_path | convertfrom-json)
 
 $loglist = "Security","System","Application","Microsoft-Windows-PowerShell/Operational"
 
-
-#Configure the array to set audit policy settings
-#Fields are as follows:  @("<category>","<subcategory>","<success>","<failure>")
-#Use wildcards to apply settings globally for the parameter. 
-#For example ("Account Logon","*","enable","disable"),
-#applies all "Account logon" subcategories success events.
-#Don't specify a category if specifying subcategories
-#Defaults below are set to MS Audit Policy "Stronger Recommendations" best practices:
-#https://github.com/MicrosoftDocs/windowsserverdocs/blob/master/WindowsServerDocs/identity/ad-ds/plan/security-best-practices/Audit-Policy-Recommendations.md
-#Future plan is to wrap this into a config file and upload different types of config files to the repo
+<#
+Configure the array to set audit policy settings
+Fields are as follows:  @("<category>","<subcategory>","<success>","<failure>")
+Use wildcards to apply settings globally for the parameter. 
+For example ("Account Logon","*","enable","disable"),
+applies all "Account logon" subcategories success events.
+Don't specify a category if specifying subcategories
+Defaults below are set to MS Audit Policy "Stronger Recommendations" best practices:
+https://github.com/MicrosoftDocs/windowsserverdocs/blob/master/WindowsServerDocs/identity/ad-ds/plan/security-best-practices/Audit-Policy-Recommendations.md
+Future plan is to wrap this into a config file and upload different types of config files to the repo
+#>
 $audit_settings = @(
     ,@("Account Logon","*","enable","enable")
 	,@("","Computer Account Management","enable","enable")
@@ -38,6 +107,7 @@ $audit_settings = @(
 	
 
 )
+
 
 
 #First, collect the current log configurations and store them in a temp file to restore later.  
@@ -76,9 +146,221 @@ $audit_config = 'auditpol /set {0} {1} /success:"{2}" /failure:"{3}" ' -f $categ
 Invoke-Expression -Command:$audit_config
 }
 
+
+
+<#
+If configuration specifies that file auditing should be enabled, read from
+the file audit configuration file and create auditing ACLs on the files of interest. 
+#>
+ 
+if($conf.file_auditing.enabled -eq "true") {
+Write-Host ""
+Write-Host "######################"
+Write-Host "########################"
+Write-Host "##########################"
+Write-Host "########################"
+Write-Host "######################"
+Write-Host ""
+Write-Host "Configuring filesystem auditing"
+Write-Host ""
+$file_conf = gc $conf.file_auditing.path
+$audit_type = "file"
+foreach ($line in $file_conf) {
+if ($line.StartsWith("#"))
+{}
+elseif ($line.StartsWith("%")) {
+$account_list = $line.split("%")[1]
+$rights = $line.split("%")[2]
+$inheritance = $line.split("%")[3]
+$propagation = $line.split("%")[4]
+$audit = $line.split("%")[5]
+} 
+elseif ($line.StartsWith("X")) {
+$current_path = $line.Substring(1)
+$error.clear()
+try {
+remove_object_audit -object_path $current_path -accounts $account_list -auditrights $rights -inheritanceflags $inheritance -propagationflags $propagation -auditflags $audit -type $audit_type
+} catch {
+"Error removing auditing on $current_path"
+ }
+if (!$error) {
+Write-Host "Removed auditing for $current_path"
+}
+}
+else {
+$current_path = $line
+$error.clear()
+try {
+set_object_audit -object_path $current_path -accounts $account_list -auditrights $rights -inheritanceflags $inheritance -propagationflags $propagation -auditflags $audit -type $audit_type
+} catch {
+"Error setting auditing on $current_path"
+ }
+if (!$error) {
+Write-Host "Successfully set auditing for $current_path"
+}
+}
+}
+}
+
+
+<#
+If configuration specifies that registry auditing should be enabled, read from
+the registry audit configuration file and create auditing ACLs on the registry keys of interest.
+#>
+ 
+if($conf.registry_auditing.enabled -eq "true") {
+Write-Host ""
+Write-Host "######################"
+Write-Host "########################"
+Write-Host "##########################"
+Write-Host "########################"
+Write-Host "######################"
+Write-Host ""
+Write-Host "Configuring registry auditing"
+Write-Host ""
+$reg_conf = gc $conf.registry_auditing.path
+$audit_type = "reg"
+#HKU is not registered as a PSDrive by default:
+New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS -ErrorAction SilentlyContinue
+#Extract SIDs for interactive users
+$usersids = $(gci hku:).name | select-String -Pattern 'S-1-5-21' | select-String -notmatch "Classes"
+foreach ($line in $reg_conf) {
+if ($line.StartsWith("#"))
+{}
+elseif ($line.StartsWith("%")) {
+$account_list = $line.split("%")[1]
+$rights = $line.split("%")[2]
+$inheritance = $line.split("%")[3]
+$propagation = $line.split("%")[4]
+$audit = $line.split("%")[5]
+} 
+elseif ($line.StartsWith("X")) 
+{
+$current_path = $line.Substring(1)
+remove_object_audit -object_path $current_path -accounts $account_list -auditrights $rights -inheritanceflags $inheritance -propagationflags $propagation -auditflags $audit -type $audit_type
+Write-Host "Removing auditing for $current_path"
+}
+elseif ($line.StartsWith("HKU:\<SID>")) 
+{
+$fragment_path = $line.Substring(10)
+foreach ($sid in $usersids) {
+$sid_value = $sid.tostring().substring(11)
+$current_path = "HKU:\$sid_value$fragment_path"
+$error.clear()
+try {
+set_object_audit -object_path $current_path -accounts $account_list -auditrights $rights -inheritanceflags $inheritance -propagationflags $propagation -auditflags $audit -type $audit_type
+} catch {
+"Error setting auditing on $current_path"
+ }
+if (!$error) {
+Write-Host "Successfully set auditing for $current_path"
+}
+}
+}
+else {
+$current_path = $line
+$error.clear()
+try {
+set_object_audit -object_path $current_path -accounts $account_list -auditrights $rights -inheritanceflags $inheritance -propagationflags $propagation -auditflags $audit -type $audit_type
+} catch {
+"Error setting auditing on $current_path"
+ }
+if (!$error) {
+Write-Host "Successfully set auditing for $current_path"
+}
+}
+
+}
+}
+
+
 Write-Host "Custom audit settings enabled for $time_period seconds"
 #Invoke sleep to allow logs to be collected for specified period of time
 Start-Sleep -s $time_period
+Write-Host ""
+Write-Host "######################"
+Write-Host "########################"
+Write-Host "##########################"
+Write-Host "########################"
+Write-Host "######################"
+Write-Host ""
+Write-Host "Restoring original audit settings"
+
+#Remove file and registry auditing, if configured:
+if($conf.file_auditing.enabled -eq "true") {
+$file_conf = gc $conf.file_auditing.path
+$audit_type = "file"
+foreach ($line in $file_conf) {
+if (($line.StartsWith("#")) -or ($line.StartsWith("X")))
+{}
+elseif ($line.StartsWith("%")) {
+$account_list = $line.split("%")[1]
+$rights = $line.split("%")[2]
+$inheritance = $line.split("%")[3]
+$propagation = $line.split("%")[4]
+$audit = $line.split("%")[5]
+} 
+else {
+$current_path = $line
+$error.clear()
+try {
+remove_object_audit -object_path $current_path -accounts $account_list -auditrights $rights -inheritanceflags $inheritance -propagationflags $propagation -auditflags $audit -type $audit_type
+} catch {
+"Error removing auditing on $current_path"
+ }
+if (!$error) {
+Write-Host "Successfully removed auditing for $current_path"
+}
+}
+}
+}
+
+
+if($conf.registry_auditing.enabled -eq "true") {
+$reg_conf = gc $conf.registry_auditing.path
+$audit_type = "reg"
+foreach ($line in $reg_conf) {
+if (($line.StartsWith("#")) -or ($line.StartsWith("X")))
+{}
+elseif ($line.StartsWith("%")) {
+$account_list = $line.split("%")[1]
+$rights = $line.split("%")[2]
+$inheritance = $line.split("%")[3]
+$propagation = $line.split("%")[4]
+$audit = $line.split("%")[5]
+} 
+
+elseif ($line.StartsWith("HKU:\<SID>")) 
+{
+$fragment_path = $line.Substring(10)
+foreach ($sid in $usersids) {
+$sid_value = $sid.tostring().substring(11)
+$current_path = "HKU:\$sid_value$fragment_path"
+$error.clear()
+try {
+remove_object_audit -object_path $current_path -accounts $account_list -auditrights $rights -inheritanceflags $inheritance -propagationflags $propagation -auditflags $audit -type $audit_type
+} catch {
+"Error removing auditing on $current_path"
+ }
+if (!$error) {
+Write-Host "Successfully removed auditing for $current_path"
+}
+}
+}
+else {
+$current_path = $line
+$error.clear()
+try {
+remove_object_audit -object_path $current_path -accounts $account_list -auditrights $rights -inheritanceflags $inheritance -propagationflags $propagation -auditflags $audit -type $audit_type
+} catch {
+"Error removing auditing on $current_path"
+ }
+if (!$error) {
+Write-Host "Successfully removed auditing for $current_path"
+}
+}
+}
+}
 
 #Restore log configurations to original state
 foreach ($logname in $loglist) {
